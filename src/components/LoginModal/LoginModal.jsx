ec3c57da1ac7a19bcd18de4b6dedad61c0278b6c
AZAT MANAS <ZTMANAS@GMAIL.COM>
2024-12-20 14:22:26 -0500

    auth.js created

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,3 +1,42 @@
-function LoginModal() {}
+import React, { useState } from "react";
+function LoginModal({ onLogin, onClose }) {
+  const { email, setEmail } = useState();
+  const { password, setPassword } = useState();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    onLogin({ email, password });
+  };
+
+  return (
+    <div className="modal">
+      <form action="submit" onSubmit={handleLogin}>
+        <h2>Login</h2>
+        <label htmlFor="input">
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Login</button>
+        <button type="cancel" onClick={onClose}>
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default LoginModal;
--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -21,7 +21,7 @@ function RegisterModal() {
 
   return (
     <ModalWithForm
-      title="New Garment"
+      title="Sign in"
       buttonText={isLoading ? "Saving..." : "Save"}
       isOpen={isOpen}
       closeActiveModal={closeActiveModal}
@@ -53,54 +53,6 @@ function RegisterModal() {
           onChange={handleChange}
         />
       </label>
-
-      <fieldset className="modal__radio-fieldset">
-        <legend className="modal__legend">Select the weather type:</legend>
-        <section className="modal__radio-buttons">
-          <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input
-              id="hot"
-              type="radio"
-              className="modal__radio-input"
-              name="weather"
-              value="hot"
-              checked={values.weather === "hot"}
-              onChange={handleChange}
-            />
-            Hot
-          </label>
-          <label
-            htmlFor="warm"
-            className="modal__label modal__label_type_radio"
-          >
-            <input
-              id="warm"
-              type="radio"
-              className="modal__radio-input"
-              name="weather"
-              value="warm"
-              checked={values.weather === "warm"}
-              onChange={handleChange}
-            />
-            Warm
-          </label>
-          <label
-            htmlFor="cold"
-            className="modal__label modal__label_type_radio"
-          >
-            <input
-              id="cold"
-              type="radio"
-              className="modal__radio-input"
-              name="weather"
-              value="cold"
-              checked={values.weather === "cold"}
-              onChange={handleChange}
-            />
-            Cold
-          </label>
-        </section>
-      </fieldset>
     </ModalWithForm>
   );
 }
