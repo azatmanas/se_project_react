6a71c228ef113069c9a1682eb7bd5fa5e57d54d0
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-03-06 11:18:47 -0500

    footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__name">Developed by Azat Manas</p>
-      <p className="footer__year">2024</p>
+      <p className="footer__year">2025</p>
     </footer>
   );
 }
