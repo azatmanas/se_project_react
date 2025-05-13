c6675d4b2f793187ce96afeebd132e531e14764b
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-05-13 19:24:18 -0400

    utils/auth js created

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
-// const baseUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "put the URL for your deployed backend here, including https://"
-//     : "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "put the URL for your deployed backend here, including https://"
+    : "http://localhost:3001";
 import { checkResponse } from "./api";
 export const register = ({ name, avatar, email, password }) => {
   return fetch(`${baseUrl}/signup`, {
